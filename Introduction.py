788b41db18de59d9e0e274c2a8b7141a7900bcf5
Tushar Fernandez <82432620+tusharfernandez@users.noreply.github.com>
2024-04-21 13:29:29 +0530

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,50 @@
+# print("My Name is Tushar")
+
+# print("what is your age?")
+var=int(input("What is your age"))
+# print(var)
+# print(type(var))
+#
+# mylist=[]
+# mylist.extend((5,7))
+# print(mylist)
+# mylist.insert(1,6)
+# print(mylist)
+# print(mylist.index(6))
+
+# mylist.append(5)
+# print(mylist)
+# myDict={}
+# print(type(myDict))
+# tuple=(1,3,5,6)
+# print(tuple)
+
+
+
+#
+# var=["tusharfernandez","ghgjh"]
+# # # print(var[0:6])
+# # # print(var[7:16])
+# #
+# # print(var[0].upper())
+# # print(type(var[0]))
+# var.clear()
+# print(var)
+#
+# for i in range(1,11):
+#     print(i)
+# var1={"tushar","Fernandez"}
+# var2={"tushar","Fernandez",56,87}
+# # var2.discard(56)                                        removing value in set
+# var38)
+# print(var3)=var1.intersection(var2)
+# var3.add(69)
+# var3.add(6
+# print(type(var1))
+# print(type(var2))
+#
+# dict1={3:"tushar",2:"fernandez"}
+# # print(len(dict1))
+# # var1=dict1.pop(2)
+# # print(var1)
+# print(dict1.keys())
