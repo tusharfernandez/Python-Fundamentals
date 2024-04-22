1693b17d7e60ff8c58600cef0a52a5821d5e9dc7
Tushar Fernandez <82432620+tusharfernandez@users.noreply.github.com>
2024-04-22 15:05:57 +0530

    Add files via upload

---
+++ Condandloops.py
@@ -0,0 +1,14 @@
+# for i in range(8):
+#     var=int(input("Enter value\t\n"))
+#     if var==0:
+#         print("0 to not execute")
+#     elif var==1:
+#         print("1 entered")
+#         for j in range(1,11):
+#             print("7 *",j ,"=", 7*j)
+
+for i in range(1,6):
+    if i==3:
+        continue
+    print(i)
+
